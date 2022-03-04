9e5793cc5caa957524fd9abc37e58af0f6b2de4d
임서영 <sylim0318@naver.com>
2022-03-04 15:07:26 +0900

    [ADD] refresh token repository

---
+++ src/main/java/com/nrjb/user/entity/RefreshTokenRepository.java
@@ -0,0 +1,11 @@
+package com.nrjb.user.entity;
+
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface RefreshTokenRepository extends CrudRepository<RefreshToken, String> {
+    Optional<RefreshToken> findByRefreshToken(String refreshToken);
+}
