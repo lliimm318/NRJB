e56ccd325b111159dda468e3bb15d33965cad15a
임서영 <sylim0318@naver.com>
2022-03-04 15:12:37 +0900

    [ADD] token response

---
+++ src/main/java/com/nrjb/user/payload/response/TokenResponse.java
@@ -0,0 +1,18 @@
+package com.nrjb.user.payload.response;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class TokenResponse {
+
+    private String accessToken;
+
+    private String refreshToken;
+
+}
