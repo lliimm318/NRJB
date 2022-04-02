c0ed95aa9a4843902f00cc14f089c428bf815f79
임서영 <sylim0318@naver.com>
2022-04-03 00:16:03 +0900

    [fix] 오타 수정

--- src/main/java/com/nrjb/exception/ErrorCode.java
+++ src/main/java/com/nrjb/exception/ErrorCode.java
@@ -1,18 +1,21 @@
 package com.nrjb.exception;
 
-import lombok.AllArgsConstructor;
 import lombok.Getter;
 
 @Getter
-@AllArgsConstructor
 public enum ErrorCode {
 
-    INVALID_TOKEN(401, "invaild token"),
+    INVALID_TOKEN(401, "invalid token"),
     USER_NOT_FOUND(404,"user not found"),
     USER_ALREADY(409, "user already");
 
     private final int status;
 
     private final String message;
+
+    ErrorCode(int status, String message) {
+        this.status = status;
+        this.message = message;
+    }
 
 }
